f30c8a11c66c9c0e257c6c6bf2f1cc90e4f0bd00
Sean <mac@Seans-MBP.hitronhub.home>
2019-07-27 01:05:32 -0500

    Added useMedia for media queries

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React from 'react';
 import { Switch, Route, Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
+import useScreenSize from './hooks/useScreenSize'
+
 import { Button } from '../src/components/Button';
 
 let history = createBrowserHistory();
 
 export default () => {
+  const [screenSize] = useScreenSize();
+  console.log(screenSize)
   return (
     <Router history={history}>
       <Switch>
